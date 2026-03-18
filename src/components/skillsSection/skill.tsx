4dd5fb8add129006665fb19d62238e6b1d79f96e
Daniel <di.vera@uc.cl>
2026-03-18 12:05:11 -0300

    style(skill section): Se hace responsiva la primera parte de la skill section

--- src/components/skillsSection/skill.tsx
+++ src/components/skillsSection/skill.tsx
@@ -9,10 +9,10 @@ export default function Skills() {
     <section
       id="skills"
       aria-labelledby="skills-title"
-      className="h-screen w-full flex justify-center items-center flex-col"
+      className="2xl:py-0 py-26 px-4 lg:px-8 min-h-screen md:min-h-fit 2xl:min-h-screen w-full flex justify-center items-center flex-col"
     >
       <motion.h2
-        className="skill-title-section text-[4.5rem] font-semibold"
+        className="skill-title-section text-[3rem] lg:text-[4.5rem] font-semibold"
         id="skills-title"
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -22,7 +22,7 @@ export default function Skills() {
         Mis <span className="text-transparent bg-clip-text">Habilidades</span>
       </motion.h2>
       <motion.p
-        className="skill-subtitle text-lg mb-16"
+        className="skill-subtitle text-lg mb-16 text-center"
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
@@ -30,14 +30,14 @@ export default function Skills() {
       >
         Tecnologías y herramientas con las que trabajo para crear soluciones excepcionales
       </motion.p>
-      <div className="flex flex-wrap gap-8 w-3/4 justify-center">
+      <div className="flex flex-wrap gap-8 w-full xl:w-3/4 justify-center">
         {skills.map((skill, index) => {
           const Icon = skill.icon;
           const tech = skill.skillTech.join(', ');
           return (
             <motion.article
               key={index}
-              className="skill-container flex w-[30%] flex-col p-8 rounded-3xl text-center items-center hover:scale-105 transition-all duration-300 ease-in-out"
+              className="skill-container flex w-full sm:w-[45%] lg:w-[30%] flex-col p-8 rounded-3xl text-center items-center hover:scale-105 transition-all duration-300 ease-in-out"
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               viewport={{ once: true }}
